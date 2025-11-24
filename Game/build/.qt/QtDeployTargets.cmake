e220be1bd315a374e8009b593f24a614b63fcfec
Michael <mikiszotkowski@protonmail.com>
2025-11-24 21:19:04 +0100

    gitignore imporve

--- Game/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Game_FILE /Users/ales/CPP/CPP_PROJEKT/Game/build/Game.app/Contents/MacOS/Game)
-set(__QT_DEPLOY_TARGET_Game_TYPE EXECUTABLE)
--- Game/build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Game_FILE /Users/ales/CPP/CPP_PROJEKT/Game/build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/Game.app/Contents/MacOS/Game)
-set(__QT_DEPLOY_TARGET_Game_TYPE EXECUTABLE)
